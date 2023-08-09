0cc84a651c34bd8b15eb7af6857f0d66694ad330
drumfeet <drumfeet@gmail.com>
2023-08-09 14:19:12 +0800

    dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,121 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Card, Title, BarChart, Subtitle } from "@tremor/react"
+import { useEffect, useState } from "react"
 
 export default function Home() {
+  const versions = [
+    "v0.27.0",
+    "v0.26.4",
+    "v0.26.3",
+    "v0.26.2",
+    "v0.26.1",
+    "v0.26.0",
+    "v0.25.0",
+    "v0.24.0",
+    "v0.23.0",
+    "v0.22.0",
+    "v0.21.0",
+    "v0.20.0",
+    "v0.19.0",
+    "v0.18.0",
+    "v0.17.0",
+    "v0.16.0",
+    "v0.15.0",
+    "v0.14.0",
+    "v0.13.0",
+    "v0.12.0",
+    "v0.11.0",
+    "v0.10.0",
+    "v0.9.0",
+    "v0.8.0",
+    "v0.7.0",
+  ]
+  const [chartData, setChartData] = useState([])
+  const [contractCount, setContractCount] = useState([])
+
+  const dataFormatter = (number) => {
+    return Intl.NumberFormat("us").format(number).toString()
+  }
+
+  useEffect(() => {
+    const srcTxIds = [
+      "jsZqVEOGdMFAvVlof_WXi6DO5vWxhteiG91xVPiIwqQ",
+      "QhbIPso1lx8wbHx4c7225L9PkNYh-Djp6N_YCjqdr_E",
+      "rTp2E6oipzJODmAGbqWbo2wzagoV7tt3JRyBsyVgo6A",
+      "-TBbRLWsP8wAlj8y5bh7RHRdwGJ7kT9urFvEbn1UomQ",
+      "OSYm83qQFF5gf4m3BbdZtrHMHjVAMPJcBhfZiJVYjYE",
+      "zdP_QTSZ2zO9Nxa1sPAKhyR4geua1_a621_fZm2XPKU",
+      "_156pyhqtRr6Zh3KX-Hp4q3-CIrMa2leTqKdu2HpXB8",
+      "fuSpwZIxJtIq3eTdbRgBSwsFYd5f6oMwKB91RR3hXOo",
+      "_gUR1-XzkZhsMlzZLIUEYp-rg73b9W-bhSrRIb06zKk",
+      "JglKj1PoKu1moG7H3uAP1HxnXRH4kDIuqQzil1ZlbLc",
+      "nNz22bZG_Y2K9r68iqnL1iOPEu8rvqCWNE4TX17OsgA",
+      "pfzMiEGWwoyAL33M2ceRUkgG3XvxUyjxUqiyLNHD66g",
+      "jhQ9kWIqjNYzGrg96zr7q7xbot4NwkKT8UZwsrb-fvE",
+      "fHH99N1FIxkU-vYwbg30eYHpBpOjN_Qa3k3ch73Yz04",
+      "WEFEoY33ntimvQzUtC7bS3A1bsGRrtXST_z9E8yx9yw",
+      "viUyq-GD9kxDYRVkvT-NPjEvzLIgQYEx70DoliFwytQ",
+      "ZRREoTw94icjJVyVnCHnR7T_Q96AWLLLnYugmt9OgAQ",
+      "ThKJQwNBy2tdELecqmlG86bo9NacyMxquQef1DOLBPA",
+      "4lSfFFQIpX37GMdab6c4ZdWli33b70qu_KJan5vB1ZI",
+      "8dUFGgl05GiunNN_5LMBYEorkS2Znr1-L2JYVb0Cpm4",
+      "9vwjxsX0856iTRFsEMEXBC7UFJ3Utok_e6dFyB1s4TA",
+      "F-nDTtI50sJYDJyPq3cqnQg2UApu9_bBoy8NItEPPQI",
+      "eufj3L8Qx1JPLVnoJHWRNNj5FvJ4E8OT07MZ9BhmpT8",
+      "cvDUleFkH8v_hU-pBwInngotLszGpUGF-e_Ask6juwI",
+      "7vXOxkxZ_eG0mwBO4pc_mB_oh1MY4pmHXzRQJfdMGCw",
+    ]
+
+    const fetchData = async (srcTxId) => {
+      try {
+        const response = await fetch(
+          `https://gateway.warp.cc/gateway/contracts-by-source?id=${srcTxId}`
+        )
+        const jsonData = await response.json()
+        return jsonData
+      } catch (error) {
+        console.error("Error fetching data:", error)
+        return null
+      }
+    }
+
+    ;(async () => {
+      try {
+        const newData = await Promise.all(
+          srcTxIds.map(async (srcTxId, index) => {
+            const data = await fetchData(srcTxId)
+            return {
+              name: versions[index],
+              "Number of deployed database": data?.paging?.items,
+            }
+          })
+        )
+
+        setChartData(newData)
+      } catch (e) {
+        console.error(e)
+      }
+    })()
+  }, [])
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <>
+      <Card>
+        <Title>WeaveDB</Title>
+        <Subtitle>
+          Contract versions with their corresponding number of deployed database
+        </Subtitle>
+        <BarChart
+          className="mt-6"
+          data={chartData}
+          index="name"
+          categories={["Number of deployed database"]}
+          colors={["purple"]}
+          valueFormatter={dataFormatter}
+          yAxisWidth={48}
+          showXAxis={false}
+          noDataText="Fetching Data....."
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </Card>
+    </>
   )
 }
