57e431490b22c747212c015c3d8041e640362efc
drumfeet <drumfeet@gmail.com>
2023-08-17 15:31:19 +0800

    background gradient color

---
+++ src/components/Background.tsx
@@ -0,0 +1,29 @@
+// components/BackgroundSvg.js
+export default function Background() {
+  return (
+    <>
+      <svg
+        width="100%"
+        height="100%"
+        viewBox="0 0 width height"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect width="100%" height="100%" fill="url(#paint0_linear_603_1118)" />
+        <defs>
+          <linearGradient
+            id="paint0_linear_603_1118"
+            x1="1512"
+            y1="-0.000237259"
+            x2="-132.706"
+            y2="617.796"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stop-color="#081228" />
+            <stop offset="1" stop-color="#330933" />
+          </linearGradient>
+        </defs>
+      </svg>
+    </>
+  )
+}
